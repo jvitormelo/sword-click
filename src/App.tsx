c9552cda8b8de4813c74263c337c104e72d271d8
jvitormelo-t <jvitor.ovidio2@hotmail.com>
2023-10-14 11:21:01 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex h-screen items-center justify-center w-full">
+      <div className="text-xl">a</div>
+    </main>
+  );
+}
+
+export default App;
