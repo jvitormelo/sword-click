43e708ae2b1986ea3b5ba517f34b5c17ccadbf4f
jvitormelo-t <jvitor.ovidio2@hotmail.com>
2023-10-19 16:58:37 -0300

    better modal

--- src/providers/modal-provider.tsx
+++ src/providers/modal-provider.tsx
@@ -1,4 +1,5 @@
 import { useModalStore } from "@/hooks/useOpenModal";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const ModalProvider = () => {
   const { isOpen, modalProps, actions } = useModalStore();
@@ -9,38 +10,43 @@ export const ModalProvider = () => {
         className={`fixed z-40 inset-0 bg-black bg-opacity-50`}
         hidden={!isOpen}
       />
-      <dialog
-        open={isOpen}
-        className={`z-50  p-4 my-auto rounded-md min-w-[30lvw] max-h-[80lvh] max-w-[50vw] flex flex-col ${
-          isOpen ? "inset-0" : ""
-        }`}
-      >
-        <button
-          onClick={actions.close}
-          className="absolute p-1 text-white border-white top-0 right-0 m-4 hover:text-gray-900 hover:!fill-amber-800"
+      <AnimatePresence>
+        <motion.dialog
+          key={"modal"}
+          animate={{ scale: isOpen ? 1 : 0.5, inset: isOpen ? 0 : undefined }}
+          exit={{ scale: [1, 0] }}
+          open={isOpen}
+          className={`z-50  p-4 my-auto rounded-md min-w-[30lvw] max-h-[80lvh] max-w-[50vw] flex flex-col`}
         >
-          <svg
-            className="w-2 h-2"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+          <button
+            onClick={actions.close}
+            className="absolute p-1 text-white border-white top-0 right-0 m-4 hover:text-gray-900 hover:!fill-amber-800"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
+            <svg
+              className="w-2 h-2"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
 
-        <div className="pt-6"></div>
+          <div className="pt-6"></div>
 
-        <h1 className="text-xl text-white text-center">{modalProps?.title}</h1>
+          <h1 className="text-xl text-white text-center">
+            {modalProps?.title}
+          </h1>
 
-        <div className="pt-4">{modalProps?.body}</div>
-      </dialog>
+          <div className="pt-4">{modalProps?.body}</div>
+        </motion.dialog>
+      </AnimatePresence>
     </>
   );
 };
